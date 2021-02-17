e700906c7973c235556ec9bf20438456559b2d1d
Matthieu Coquet <matthieu.coquet@outlook.com>
2021-02-17 23:40:45 +0900

    Curved ui + 2 sample in center

--- scenes/tournesol/shaders/ui.glsl
+++ scenes/tournesol/shaders/ui.glsl
@@ -10,8 +10,15 @@ float sdbox(in vec3 position, in vec3 half_sides)
 
 Hit map(in vec3 position)
 {
-    float d = sdbox(position, vec3(0.49, 0.49, 0.001)) - 0.01;
-    return Hit(d, UNKNOW);
+    float radius = 2.0;
+    float height = 0.5;
+    float distance = length(position.xz - vec2(0.0, radius)) - radius;
+    distance = abs(distance) - 0.001;
+    vec2 w = vec2(distance, abs(position.y) - height);
+    distance = min(max(w.x, w.y), 0.0) + length(max(w, 0.0));
+    distance = max(distance, position.z - 0.5);
+    distance = max(distance, abs(position.x) - 0.5) - 0.001;
+    return Hit(distance, UNKNOW);
 }
 
 Material get_color(in vec3 position)
@@ -21,3 +28,5 @@ Material get_color(in vec3 position)
     uv.y *= -1;
     return Material(textureLod(ui, nonuniformEXT(uv), 0.0).xyz, 4);
 }
+
+
